3cb47dd7482a2abf916c9356cf926482579daec3
Kyle Fuller <kyle@fuller.li>
2016-01-15 19:59:06 +0000

    [release] Support CocoaPods Ruby specs

--- maintain/release/cocoapods.py
+++ maintain/release/cocoapods.py
@@ -1,4 +1,5 @@
 import json
+import re
 import collections
 from glob import glob
 
@@ -10,6 +11,8 @@ from maintain.process import invoke
 
 class CocoaPodsReleaser(Releaser):
     name = 'CocoaPods'
+    RUBY_VERSION_REGEX = re.compile(r'\.version\s*=\s*["\'](.*)["\']')
+    RUBY_SUB_VERSION_REGEX = re.compile(r'(\.version\s*=\s*["\']).*(["\'])')
 
     @classmethod
     def podspecs(cls):
@@ -33,24 +36,60 @@ class CocoaPodsReleaser(Releaser):
                             ' permitted.'.format(', '.join(podspecs)))
 
         self.podspec = podspecs[0]
-
-        if not self.podspec.endswith('.json'):
-            # TODO Add support for Ruby Podspecs
-            raise Exception('Ruby podspecs are currently unsupported.')
+        self.is_json_podspec = self.podspec.endswith('.json')
 
     def determine_current_version(self):
         with open(self.podspec) as fp:
-            spec = json.load(fp)
-            return Version(spec['version'])
+            if self.is_json_podspec:
+                return self.determine_current_version_json_podspec(fp)
+
+            return self.determine_current_version_ruby_podspec(fp)
+
+    def determine_current_version_json_podspec(self, fp):
+        spec = json.load(fp)
+        return Version(spec['version'])
+
+    def determine_current_version_ruby_podspec(self, fp):
+        match = self.RUBY_VERSION_REGEX.search(fp.read())
+        if match:
+            return Version(match.groups()[0])
+        else:
+            raise Exception('Invalid podspec ({}), doesn\'t contain ' +
+                            'a version.'.format(self.podspec))
 
     def bump(self, new_version):
+        if self.is_json_podspec:
+            self.bump_json_podspec(new_version)
+        else:
+            self.bump_ruby_podspec(new_version)
+
+    def bump_json_podspec(self, new_version):
         with open(self.podspec) as fp:
             spec = json.load(fp, object_pairs_hook=collections.OrderedDict)
             spec['version'] = str(new_version)
+            source = spec['source']
+            if 'tag' not in source:
+                raise Exception('JSON podspec {} doesn\'t have a git ' +
+                                'tag'.format(self.podspec))
+
+            source['tag'] = str(new_version)
+            spec['source'] = source
 
         with open(self.podspec, 'w') as fp:
             json.dump(spec, fp, indent=2, separators=(',', ': '))
             fp.write('\n')
+
+    def bump_ruby_podspec(self, new_version):
+        with open(self.podspec, 'r') as fp:
+            def replace(matcher):
+                return '{}{}{}'.format(matcher.group(1), new_version,
+                                       matcher.group(2))
+
+            content = self.RUBY_SUB_VERSION_REGEX.sub(replace, fp.read(), count=1)
+
+        with open(self.podspec, 'w') as fp:
+            print(content)
+            fp.write(content)
 
     def release(self):
         invoke(['pod', 'trunk', 'push', self.podspec])
--- tests/release/test_cocoapods.py
+++ tests/release/test_cocoapods.py
@@ -12,9 +12,9 @@ from ..utils import temp_directory, touch
 class CocoaPodsReleaserTestCase(unittest.TestCase):
     # Detection
 
-    def test_detects_podspec(self):
+    def test_detects_ruby_podspec(self):
         with temp_directory() as directory:
-            touch('URITemplate.podspec')
+            touch('JSONWebToken.podspec')
             self.assertTrue(CocoaPodsReleaser.detect())
 
     def test_detects_json_podspec(self):
@@ -24,10 +24,18 @@ class CocoaPodsReleaserTestCase(unittest.TestCase):
 
     # Check current version
 
-    def test_detect_current_version(self):
+    def test_detect_current_version_ruby_podspec(self):
+        fixture_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fixtures')
+        podspec = os.path.join(fixture_path, 'JSONWebToken.podspec')
+
+        with temp_directory() as directory:
+            shutil.copyfile(podspec, 'JSONWebToken.podspec')
+            version = CocoaPodsReleaser().determine_current_version()
+            self.assertEqual(version, Version('1.4.1'))
+
+    def test_detect_current_version_json_podspec(self):
         fixture_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fixtures')
         podspec = os.path.join(fixture_path, 'URITemplate.podspec.json')
-        bumped_podspec = os.path.join(fixture_path, 'BumpedURITemplate.podspec.json')
 
         with temp_directory() as directory:
             shutil.copyfile(podspec, 'URITemplate.podspec.json')
@@ -36,19 +44,15 @@ class CocoaPodsReleaserTestCase(unittest.TestCase):
 
     # Verify
 
-    def test_passes_validation(self):
+    def test_passes_validation_with_json_spec(self):
         with temp_directory() as directory:
             touch('URITemplate.podspec.json')
             CocoaPodsReleaser()
 
-    def test_fails_validation_with_ruby_podspecs(self):
-        # TODO: Ruby podspecs should be somehow supported in the future
-
+    def test_passes_validation_with_ruby_spec(self):
         with temp_directory() as directory:
-            touch('URITemplate.podspec')
-
-            with self.assertRaises(Exception):
-                CocoaPodsReleaser()
+            touch('JSONWebToken.podspec')
+            CocoaPodsReleaser()
 
     def test_fails_validation_with_multiple_podspecs(self):
         with temp_directory() as directory:
@@ -69,3 +73,13 @@ class CocoaPodsReleaserTestCase(unittest.TestCase):
             shutil.copyfile(podspec, 'URITemplate.podspec.json')
             CocoaPodsReleaser().bump('1.2.1')
             self.assertTrue(filecmp.cmp('URITemplate.podspec.json', bumped_podspec))
+
+    def test_bumps_ruby_podspec(self):
+        fixture_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fixtures')
+        podspec = os.path.join(fixture_path, 'JSONWebToken.podspec')
+        bumped_podspec = os.path.join(fixture_path, 'BumpedJSONWebToken.podspec')
+
+        with temp_directory() as directory:
+            shutil.copyfile(podspec, 'JSONWebToken.podspec')
+            CocoaPodsReleaser().bump('2.0.0')
+            self.assertTrue(filecmp.cmp('JSONWebToken.podspec', bumped_podspec))
