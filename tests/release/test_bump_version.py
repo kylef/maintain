c422ee091b7c44cbc4d4b356c2b8a180205137cd
Kyle Fuller <kyle@fuller.li>
2016-05-07 22:21:27 +0000

    [Version File Releaser] Update tests

--- tests/release/test_bump_version.py
+++ tests/release/test_bump_version.py
@@ -1,20 +1,42 @@
 import os
 import unittest
 
-from maintain.commands.release import bump_version_file
+from semantic_version import Version
+
+from maintain.release.version_file import VersionFileReleaser
 from ..utils import temp_directory, touch
 
 
 class BumpVersionTestCase(unittest.TestCase):
-    def test_doesnt_write_version_file_when_non_existant(self):
+    # Detection
+
+    def test_detects_version_file(self):
         with temp_directory() as directory:
-            bump_version_file('2.3.0')
-            self.assertFalse(os.path.exists('VERSION'))
+            touch('VERSION')
+            self.assertTrue(VersionFileReleaser.detect())
+
+    def test_doesnt_detect_without_version_file(self):
+        with temp_directory() as directory:
+            self.assertFalse(VersionFileReleaser.detect())
+
+    # Determing current version
+
+    def test_determine_current_version(self):
+        releaser = VersionFileReleaser()
+
+        with temp_directory() as directory:
+            touch('VERSION', '0.2.6')
+            version = releaser.determine_current_version()
+            self.assertEqual(version, Version('0.2.6'))
+
+    # Bumping
 
     def test_bumps_version_file(self):
+        releaser = VersionFileReleaser()
+
         with temp_directory() as directory:
             touch('VERSION', '2.2.12')
-            bump_version_file('2.3.0')
+            releaser.bump('2.3.0')
 
             with open('VERSION') as fp:
-                self.assertEqual(fp.read(), '2.3.0')
+                self.assertEqual(fp.read(), '2.3.0\n')
