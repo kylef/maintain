c9f494dc0ddc46369090028b4731af932a99b168
Kyle Fuller <kyle@fuller.li>
2016-05-10 21:08:48 -0700

    Release 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 # Maintain Changelog
 
-## Master
+## 0.2.0 (2016-05-10)
 
 Initial release.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='maintain',
-    version='0.1.0',
+    version='0.2.0',
     description='maintain',
     url='https://github.com/kylef/maintain',
     packages=find_packages(),
