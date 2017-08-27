6802008fad40761eea0f863f0d1cbe7f34d19870
Kyle Fuller <kyle@fuller.li>
2017-08-26 20:44:58 -0700

    fix: Add missing test fixture for setup.py

---
+++ tests/release/fixtures/setup-no-version.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+with open('VERSION') as fp:
+    VERSION = fp.read().strip()
+
+
+setup(
+  name='changelog',
+  version=VERSION,
+  url='https://github.com/kylef/changelog',
+  author='Kyle Fuller',
+  author_email='kyle@fuller.li',
+  entry_points={
+    'console_scripts': {
+      'changelog = changelog:cli',
+    }
+  },
+  install_requires=[
+    'click',
+    'semver',
+    'commonmark',
+  ],
+  license='BSD'
+)
